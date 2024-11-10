ced3fa5759042663743a37fe61ea399e8175859c
rufus173 <derrickotron5000@gmail.com>
2024-11-10 23:07:12 +0000

    implemented linked list for request headers

--- src/http.c
+++ src/http.c
@@ -6,7 +6,18 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <string.h>
+//================ macros ==============
+#define CHECK_SEND(result,return_val) \
+if (result < 0){\
+	perror("send");\
+	return return_val;\
+}
 //=================== structs ================
+struct header {
+	char *field_name;
+	char *field_value;
+	void *next;
+};
 struct http_connection {
 	int socket;
 	char *version_string;
@@ -15,12 +26,22 @@ struct http_connection {
 struct http_request {
         char *method;
         char *url;
+	char *body;
+	struct header *header;
+};
+struct http_response {
+	char *response_line;
+	
 };
 //================== public funcitons =============
 struct http_connection *http_connect(char *host){
 	//setup returned struct
 	struct http_connection *connection;
 	connection = malloc(sizeof(struct http_connection));
+	if (connection == NULL){
+		perror("malloc");
+		return NULL;
+	}
 
 	//resolve a hostname or url
 	struct addrinfo hints, *address_info;
@@ -58,12 +79,26 @@ int http_disconnect(struct http_connection *connection){
 	return 0;
 }
 int http_send_request(struct http_connection *connection,struct http_request *request){
-	int request_line_size = snprintf(NULL,0,"%s %s HTTP/1.1",request->method,request->url)+1;//get size of request line
-	char *request_line = malloc(request_line_size);
-	snprintf(request_line,request_line_size,"%s %s HTTP/1.1",request->method,request->url);
-	
+	//setup a buffer for the full request
+	char *full_request = NULL;
+	long long int full_request_size = 0;
 
-	free(request_line);
+	//add the request line
+	int request_line_size = snprintf(NULL,0,"%s %s HTTP/1.1\r\n",request->method,request->url)+1;//get size of request line
+	full_request_size += request_line_size;
+	full_request = realloc(full_request,full_request_size);
+	snprintf(full_request,request_line_size,"%s %s HTTP/1.1\r\n",request->method,request->url);
+	
+	//mark the start of the body
+	full_request_size += 2;
+	full_request = realloc(full_request,full_request_size);
+	strcat(full_request,"\r\n");
+
+	printf("%s",full_request);
+	//send the full request packet
+	int bytes_sent = send(connection->socket,full_request,full_request_size,0);
+	free(full_request);
+
 	return 0;
 }
 struct http_request *http_generate_request(char *method, char *url){
@@ -80,11 +115,63 @@ struct http_request *http_generate_request(char *method, char *url){
 	}
 	snprintf(request->method,strlen(method)+1,"%s",method);
 	snprintf(request->url,strlen(url)+1,"%s",url);
+	request->body = NULL;
+	request->header = NULL;
 	return request;
 }
 int http_free_request(struct http_request *request){
 	free(request->method);
 	free(request->url);
+	//free the linked list of headers
+	struct header *node;
+	node = request->header;
+	for (;;){
+		if (node == NULL){
+			break;
+		}
+		struct header *prev_node = node;
+		node = prev_node->next;
+		printf("freeing header \"%s: %s\"\n",prev_node->field_name,prev_node->field_value);
+		free(prev_node->field_name);
+		free(prev_node->field_value);
+		free(prev_node);
+	}
 	free(request);
+	return 0;
+}
+struct http_response *http_receive_response(struct http_connection *connection){
+	char buffer[4096];
+	int buffer_size = sizeof(buffer);
+	recv(connection->socket,buffer,buffer_size,0);
+	//printf("%s",buffer);
+	return NULL;
+}
+int http_request_append_header(struct http_request *request, char *field_name, char *field_value){
+	//find the tail node
+	struct header *next_node;
+	next_node = request->header;
+	struct header **new_node_location = &request->header;
+	for (;;){
+		if (next_node == NULL){
+			break;
+		}
+		struct header *node;
+		node = next_node;
+		next_node = node->next;
+		new_node_location = (struct header **)&node->next;
+	}
+	//append new struct
+	struct header *new_node = malloc(sizeof(struct header));
+	if (new_node == NULL){
+		perror("malloc");
+		return -1;
+	}
+	memset(new_node, 0, sizeof(struct header));
+	new_node->next = NULL;
+	new_node->field_name = malloc(strlen(field_name)+1);
+	new_node->field_value = malloc(strlen(field_value)+1);
+	snprintf(new_node->field_name,strlen(field_name)+1,"%s",field_name);
+	snprintf(new_node->field_value,strlen(field_value)+1,"%s",field_value);
+	*new_node_location = new_node;
 	return 0;
 }
--- src/http.h
+++ src/http.h
@@ -7,10 +7,20 @@ struct http_connection {
 	char *version_string;
 	struct addrinfo *address_info;
 };
+struct header {
+        char *field_name;
+        char *field_value;
+        void *next;
+};
 struct http_request {
 	char *method;
 	char *url;
+	char *body;
+	struct header *header;
 };
+struct http_response {
+};
+typedef struct http_response HTTP_response;
 typedef struct http_request HTTP_request;
 typedef struct http_connection HTTP_connection;
 struct http_connection *http_connect(char *host);
@@ -18,4 +28,6 @@ int http_disconnect(HTTP_connection *);
 int http_send_request(HTTP_connection *connection,HTTP_request *request);
 HTTP_request *http_generate_request(char *method,char *url);
 int http_free_request(HTTP_request *request);
+HTTP_response *http_receive_response(HTTP_connection *);
+int http_request_append_header(HTTP_request *, char *field_name, char *field_value);
 #endif
