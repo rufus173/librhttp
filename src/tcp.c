c39573c3d4c73669a3cc80df92c177271fe1c23e
rufus173 <rufus09173@gmail.com>
2024-12-02 08:34:55 +0000

    i dont think i tested the last commit

--- src/tcp.c
+++ src/tcp.c
@@ -1,8 +1,12 @@
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
 int tcp_recvall(int socket,char *buffer,size_t max){
 	size_t bytes_left = max;
 	for (;bytes_left <= 0;){
-		int result = recv(buffer,bytes_left,0);
+		int result = recv(socket,buffer,bytes_left,0);
 		if (result < 0){
 			perror("recv");
 			return -1;
