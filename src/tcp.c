cea18c8d0716ea91ad36519f2ffae0a2ecf6a639
rufus173 <rufus09173@gmail.com>
2024-12-04 19:55:54 +0000

    started on carrage return line feed recv

--- src/tcp.c
+++ src/tcp.c
@@ -19,3 +19,17 @@ int tcp_recvall(int socket,char *buffer,size_t max){
 	//printf("got %ld bytes\n",bytes_received);
 	return 0;
 }
+long int tcp_recv_to_crlf(int socket,char **buffer){
+	long int bytes_received = 0;
+	char *recv_buffer;
+	*buffer = malloc(1);
+	for (;;){
+		int result = recv(socket,recv_buffer,1,0);
+		if (result < 0){
+			perror("recv");
+			return -1;
+		}
+		bytes_received += result;
+		*buffer = realloc(*buffer,bytes_received);
+	}
+}
